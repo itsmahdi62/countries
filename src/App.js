97616968bc000658fc5a30c0e5d609b38c78ec97
Mahdi <amiralmasi021@gmail.com>
2023-04-01 20:20:32 +0430

    Create -Navbar and Container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import { useState } from 'react';
+import Navbar from './components/Navbar/Navbar';
 const App = () => {
   return (
     <div className="App">
-     
+     <Navbar />
      
     </div>
   );
---
+++ src/containers/RegionPicker.js
@@ -0,0 +1,22 @@
+import "../App"
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import React from "react";
+// import AfricaImage
+const RegionPicker = () => {
+    return (
+        <div>         
+    <React.Fragment>
+      <CssBaseline />
+      <Container fixed>
+        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
+            
+        </Box>
+      </Container>
+    </React.Fragment>
+        </div>
+    );
+}
+ 
+export default RegionPicker;
